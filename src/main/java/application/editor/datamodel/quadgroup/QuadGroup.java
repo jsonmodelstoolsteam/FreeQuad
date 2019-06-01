e8cfc2977d3b9158bd4c67c4fcaab7f4f67384ad
hohserg <hohserg1@yandex.ru>
2019-06-01 16:42:45 +0300

    Move to relevant package

--- src/main/java/application/editor/datamodel/quadgroup/QuadGroup.java
+++ src/main/java/application/editor/datamodel/quadgroup/QuadGroup.java
@@ -1,5 +1,6 @@
-package application.editor.datamodel;
+package application.editor.datamodel.quadgroup;
 
+import application.editor.datamodel.ModelEntry;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
